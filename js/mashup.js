9e6cc1b112bacde01b5299b89c3f9b819abe689b
Dana Weiss and Jennifer Kinsey <dana@>
2017-05-24 16:54:15 -0700

    final commit of the day

--- js/mashup.js
+++ js/mashup.js
@@ -3,9 +3,35 @@ var apiKey = require('./../.env').apiKey;
 function Potd(){
 }
 
-Potd.prototype.getPotd = function() {
+Potd.prototype.getDate = function() {
   $.get('https://api.nasa.gov/planetary/apod?api_key=' + apiKey).then(function(response) {
+    return response.date;
+  }).fail(function(error) {
+    $('.date').text("date not found");
+  });
+}
 
+Potd.prototype.getDate = function() {
+  $.get('https://api.nasa.gov/planetary/apod?api_key=' + apiKey).then(function(response) {
+    return response.date;
+  }).fail(function(error) {
+    $('.date').text("date not found");
+  });
+}
+
+Potd.prototype.getDate = function() {
+  $.get('https://api.nasa.gov/planetary/apod?api_key=' + apiKey).then(function(response) {
+    return response.date;
+  }).fail(function(error) {
+    $('.date').text("date not found");
+  });
+}
+
+Potd.prototype.getDate = function() {
+  $.get('https://api.nasa.gov/planetary/apod?api_key=' + apiKey).then(function(response) {
+    return response.date;
+  }).fail(function(error) {
+    $('.date').text("date not found");
   });
 }
 
